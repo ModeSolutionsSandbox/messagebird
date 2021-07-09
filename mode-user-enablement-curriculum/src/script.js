f58e6d96b73fe9c3fbbea27e258ffbebc852cfe6
Mode Solutions Sandbox <85627824+ModeSolutionsSandbox@users.noreply.github.com>
2021-07-09 18:26:41 -0500

    Revert "Styling-Enhance"

--- mode-user-enablement-curriculum/src/script.js
+++
@@ -1,9 +0,0 @@
-	<script>
-    // Selecting the iframe element
-    var iframe = document.getElementById("myIframe");
-    
-    // Adjusting the iframe height onload event
-    iframe.onload = function(){
-        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-    }
-	</script>
